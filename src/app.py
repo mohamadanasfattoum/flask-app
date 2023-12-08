6c523837036ba5186032815603554ee03ed3ebda
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-08 23:04:08 +0100

    add user app to get name

--- src/app.py
+++ src/app.py
@@ -5,8 +5,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/') # the app path 
-def home():
-    return 'Hello! this is the main page <h1>HELLO<h1>'
+def home():   # the first app
+    return 'Hello! this is the main page <h1>HELLO<h1>'  # the html template
+
+@app.route('/<name>')
+def user(name):
+    return f'Hello {name} !'
 
 if __name__ == '__main__':
     app.run()
