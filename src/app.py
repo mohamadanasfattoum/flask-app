cc08673cc27397ca53cb7bdb2023f6216cbe2d6b
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-09 18:05:38 +0100

    add templates folder

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for 
+from flask import Flask, redirect, url_for , render_template
 # redirect zuumleiten von url zu andern
 
 
@@ -6,16 +6,16 @@ app = Flask(__name__)
 
 @app.route('/') # the app path 
 def home():   # the first app
-    return 'Hello! this is the main page <h1>HELLO<h1>'  # the html template
+    return 'Hello! this is the main page <h1>HELLO</h1>'  # the html template
 
-@app.route('/<name>')
-def user(name):
-    return f'Hello {name} !'
+# @app.route('/<name>')
+# def user(name):
+#     return f'Hello {name} !'
 
 
-@app.route('/admin')
-def admin():
-    return redirect(url_for('user', name='Admin!'))
+# @app.route('/admin')
+# def admin():
+#     return redirect(url_for('user', name='Admin!'))
 
 
 
