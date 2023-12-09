9fb98c14ad4a8c96b65a52561665abaaf4b7adef
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-09 23:30:55 +0100

    add for loop in index.html

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/<name>') # the app path 
 def home(name):   # the first app
-    return render_template('index.html', content=name)
+    return render_template('index.html', content=name, r=2)
 
 # @app.route('/<name>')
 # def user(name):
