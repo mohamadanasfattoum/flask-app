a10c8208509073b84efeec66bce2d7d4b685ddeb
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-11 12:37:23 +0100

    used session to show user

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, redirect, url_for , render_template, request
+from flask import Flask, redirect, url_for , render_template, request, session
 # redirect zuumleiten von url zu andern
 
 
 app = Flask(__name__)
+app.secret_key= 'hallo'
+
 
 @app.route('/') # the app path 
 def home():   # the first app
@@ -11,13 +13,26 @@ def home():   # the first app
 @app.route('/login', methods=['POST', 'GET']) 
 def login ():
     if request.method == 'POST': # request اداة لحمل المعلومات وارجاعها لتستخدم في المكتبة 
-        user= request.form['nm']
-        return redirect(url_for('user', usr=user)) # wenn wir zu def user gehen, usr = User und user= bringt die data von template mit post über request
+        user = request.form['nm']
+        session['user'] = user
+        return redirect(url_for('user')) # wenn wir zu def user gehen, usr = User und user= bringt die data von template mit post über request
     else:
         return render_template ('login.html')
-@app.route('/<usr>')
-def user (usr):
-    return f'<h1>{usr}</h1>'
+
+
+@app.route('/user')
+def user ():
+    if 'user' in session:
+        user = session['user']
+        return f'<h1>{user}</h1>'
+    
+    else:
+        return redirect(url_for('login'))
+
+
+# @app.route('/<usr>')
+# def user (usr):
+#     return f'<h1>{usr}</h1>'
 
 # @app.route('/<name>')
 # def user(name):
