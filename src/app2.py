e22b9658273093f1cb4adc7e02f9ea4542125ae3
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-12 23:24:25 +0100

    install from flask_sqlalchemy import SQLAlchemy

---
+++ src/app2.py
@@ -0,0 +1,66 @@
+from flask import Flask, redirect, url_for , render_template, request, session
+# redirect zuumleiten von url zu andern
+from datetime import timedelta
+import SQLAlchemy
+
+
+
+#-----------Apps------------
+app = Flask(__name__)
+app.secret_key= 'hallo'
+app.permanent_session_lifetime = timedelta(minutes=1)
+
+
+
+#------------Apps.route-------
+
+
+@app.route('/') # the app path 
+def home():   # the first app
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST', 'GET']) 
+def login ():
+    if request.method == 'POST': # request اداة لحمل المعلومات وارجاعها لتستخدم في المكتبة 
+        session.permanent = True
+        user = request.form['nm']
+        session['user'] = user
+        return redirect(url_for('user')) # wenn wir zu def user gehen, usr = User und user= bringt die data von template mit post über request
+    else:
+        if 'user' in session:
+            return redirect (url_for('user'))
+        return render_template ('login.html')
+
+
+@app.route('/user')
+def user ():
+    if 'user' in session:
+        user = session['user']
+        return f'<h1>{user}</h1>'
+    
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect(url_for('login'))
+
+
+# @app.route('/<usr>')
+# def user (usr):
+#     return f'<h1>{usr}</h1>'
+
+# @app.route('/<name>')
+# def user(name):
+#     return f'Hello {name} !'
+
+
+# @app.route('/admin')
+# def admin():
+#     return redirect(url_for('user', name='Admin!'))
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)  # debug=True to keep the srver run
